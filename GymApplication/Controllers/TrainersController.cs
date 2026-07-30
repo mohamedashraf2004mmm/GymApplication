bb0558092f6a161f269f12c8dc869198ae50a881
Ashraffff <Ashrafff@email.com>
2026-07-30 20:14:56 +0300

    completed working on trainers controller and required views

--- GymApplication/Controllers/TrainersController.cs
+++ GymApplication/Controllers/TrainersController.cs
@@ -1,4 +1,5 @@
 ﻿using GymApplication.BLL.Services.Interfaces;
+using GymApplication.BLL.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymApplication.PL.Controllers
@@ -25,6 +26,68 @@ namespace GymApplication.PL.Controllers
             return View(trainer);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Create() => View();
 
+        [HttpPost]
+        public async Task<IActionResult>Create(CreateTrainerViewModel model , CancellationToken ct)
+        {
+            if(!ModelState.IsValid)return View(model);
+
+            var result = await _trainerService.CreateTrainerAsync(model , ct);
+            if (result)
+            {
+                TempData["SuccessMessage"] = "Trainer Created Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            TempData["ErrorMessage"] = "Failed to create Trainer";
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult>Edit(int id , CancellationToken ct)
+        {
+            var trainer = await _trainerService.GetTrainerToUpdateAsync(id);
+            if(trainer is null)
+            {
+                TempData["ErrorMessage"] = "Trainer not found";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(trainer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id , TrainerToUpdateViewModel model , CancellationToken ct)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var result = await _trainerService.UpdateTrainerDetailsAsync(id, model , ct);
+            if(result) TempData["SuccessMessage"] = "Trainer Updated Successfully";
+            else TempData["ErrorMessage"] = "Failed to Update Trainer";
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id, CancellationToken ct)
+        {
+            var trainer = await _trainerService.GetTrainerDetailsAsync(id);
+            if (trainer is null)
+            {
+                TempData["ErrorMessage"] = "Trainer not found";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id, CancellationToken ct)
+        {
+            var result = await _trainerService.RemoveTrainerAsync(id);
+            if (result) TempData["SuccessMessage"] = "Trainer Deleted Successfully";
+            else TempData["ErrorMessage"] = "Failed to delete Trainer";
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
