47f193531e904f31f3565ddd669efb9bb9397364
Ashraffff <Ashrafff@email.com>
2026-08-01 20:47:59 +0300

    began working on UnitOfWork

---
+++ GymApplication.DAL/Repositories/Classes/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using GymApplication.DAL.Data.DbContextss;
+using GymApplication.DAL.Data.Models;
+using GymApplication.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApplication.DAL.Repositories.Classes
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly GymDbContext dbContext;
+        private readonly Dictionary<string, object> _repositories =[]; 
+
+        public UnitOfWork(GymDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : BaseEntity, new()
+        {
+           //check TEntity!!
+           var typeName = typeof(TEntity).Name;
+            //if (_repositories.ContainsKey(typeName)) 
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken ct = default)
+        {
+           return await dbContext.SaveChangesAsync(ct);
+        }
+    }
+}
---
+++ GymApplication.DAL/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using GymApplication.DAL.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymApplication.DAL.Repositories.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        // public IGenericRepository<Plan> PlanRepository { get; set; } => NO
+
+        IGenericRepository<TEntity>GetRepository<TEntity>() where TEntity : BaseEntity, new();
+        Task<int> SaveChangesAsync(CancellationToken ct = default);
+    }
+}
