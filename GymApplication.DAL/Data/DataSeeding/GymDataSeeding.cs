9b29103f539f564e3dcd7b43c5c0eb29297b57d1
Ashraffff <Ashrafff@email.com>
2026-08-11 08:13:56 +0300

    empty commit

--- GymApplication.DAL/Data/DataSeeding/GymDataSeeding.cs
+++ GymApplication.DAL/Data/DataSeeding/GymDataSeeding.cs
@@ -12,41 +12,6 @@ namespace GymApplication.DAL.Data.DataSeeding
 {
     public static class GymDataSeeding
     {
-        public async static Task SeedAsync(GymDbContext dbContext , string seedFolderPath , CancellationToken ct = default)
-        {
-            try
-            {
-                if(!await dbContext.Plans.AnyAsync(ct))
-                {
-                    var plans = LoadDataFromJsonFile<Plan>(seedFolderPath , "Plans.json");
-                    if (plans.Any())
-                    {
-                        dbContext.Plans.AddRange(plans);
-                    }
-                }
-            }
-            catch
-            {
-
-            }
-        }
-
-        private static List<T>LoadDataFromJsonFile<T>(string folderPath , string fileName)
-        {
-            var filePath = Path.Combine(folderPath, fileName);
-            if (!File.Exists(filePath))
-                throw new FileNotFoundException($"Can not seed data from {filePath}");
-
-            var data = File.ReadAllText(filePath);
-
-            var options = new JsonSerializerOptions()
-            {
-                PropertyNameCaseInsensitive = true
-            };
-
-            return JsonSerializer.Deserialize<List<T>>(data, options) ?? [];
-
-        }
-
+        
     }
 }
