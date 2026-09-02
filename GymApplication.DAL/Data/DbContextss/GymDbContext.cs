7f5520c842a00157ab1a3e215fc28e09cda84575
Ashraffff <Ashrafff@email.com>
2026-09-03 01:47:50 +0300

    added security model

--- GymApplication.DAL/Data/DbContextss/GymDbContext.cs
+++ GymApplication.DAL/Data/DbContextss/GymDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using GymApplication.DAL.Data.Configurations;
 using GymApplication.DAL.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace GymApplication.DAL.Data.DbContextss
 {
-    public class GymDbContext : DbContext
+    public class GymDbContext : IdentityDbContext<ApplicationUser>
     {
 
 
@@ -22,8 +24,19 @@ namespace GymApplication.DAL.Data.DbContextss
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfiguration(new PlanConfiguration());
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.Entity<ApplicationUser>(eb =>
+            {
+                eb.Property(x => x.FirstName)
+                  .HasColumnType("varchar")
+                  .HasMaxLength(50);
+
+                eb.Property(x => x.LastName)
+                  .HasColumnType("varchar")
+                  .HasMaxLength(50);
+            });
         }
 
         public DbSet<Plan> Plans { get; set; }
@@ -35,6 +48,11 @@ namespace GymApplication.DAL.Data.DbContextss
         public DbSet<HealthRecord> HealthRecords { get; set; }
         public DbSet<MemberShip> MemberShips { get; set; }
         public DbSet<Booking> Bookings { get; set; }
+
+        public DbSet<ApplicationUser> Users { get; set; }
+        public DbSet<IdentityRole> Roles { get; set; }
+
+        public DbSet<IdentityUserRole<string>> UserRoles { get; set; }
 
 
     }
